356316bed28f7e039d2f07c421177bd24c645ad0
Swapnil Bhabal <swapnilbhabal787@gmail.com>
2022-01-29 23:55:31 +0530

    if-else-if chaining and understanding of truthy and falsy values is done.

--- js-conditions/js-conditions.js
+++ js-conditions/js-conditions.js
@@ -15,3 +15,36 @@ else {
     console.log('EVEN');
 }
 
+num = 35;
+
+if (num % 2 !== 0) {
+    console.log('Odd');
+} else if (num % 2 === 0) {
+    console.log('even');
+}
+
+age = 46;
+
+if (age > 18 && age < 30) {                 //if-else-if chaining
+    console.log('Entry granted');
+} else if (age > 30 && age < 45) {
+    console.log('Can enjoy beer'); 
+} else if (age > 45) {
+    console.log('beers are free, enjoy!!');
+}
+
+let isLoggedIn;                             //falsy value hence second condition runs
+
+if (isLoggedIn) {
+    console.log('Hello!');
+} else {
+    console.log('Please login');
+}
+
+let loggedInUser = 10;                      //truthy value hence the first condition runs
+
+if (loggedInUser) {
+    console.log('Please log in');
+} else {
+    console.log(`Welcome ${loggedInUser}`);
+}
