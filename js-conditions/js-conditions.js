a944b4d885a733994e5183ec3cd1e78d1368cc10
Swapnil Bhabal <swapnilbhabal787@gmail.com>
2022-01-29 23:42:43 +0530

    Javascript if else condition is done.

---
+++ js-conditions/js-conditions.js
@@ -0,0 +1,17 @@
+let age = 21;
+
+if (age > 18) {                         //Normal If condition
+    console.log('Entry Granted');
+}
+
+console.log(1000 + 100);
+
+let num = 32;
+
+if (num % 2 !== 0) {                    //If else condition
+    console.log('ODD');
+}
+else {
+    console.log('EVEN');
+}
+
