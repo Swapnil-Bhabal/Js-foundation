84855624b080f3f9260ef63d7f0e2f7786e8ba63
Swapnil Bhabal <swapnilbhabal787@gmail.com>
2022-02-02 00:19:26 +0530

    Reversing the string is done.

---
+++ js-arrays/03-exercise-reverse/reverseString.js
@@ -0,0 +1,7 @@
+const reverseString = function(str) {
+    let newString = str.split("").reverse().join("");
+    return newString
+};
+
+// Do not edit below this line
+module.exports = reverseString;
