3c9c59d498e11e124146b42e18293eda24af44c7
Swapnil Bhabal <swapnilbhabal787@gmail.com>
2022-02-02 00:44:19 +0530

    Removing the array value is done

--- js-arrays/03-exercise-reverse/reverseString.js
+++ js-arrays/03-exercise-reverse/reverseString.js
@@ -1,5 +1,5 @@
 const reverseString = function(str) {
-    let newString = str.split("").reverse().join("");
+    let newString = str.split("").reverse().join("");       // It first splits the string in ['a', 'b', 'c'], then reverse function --> ['c', 'b', 'a'], then join function --> 'cba'; 
     return newString
 };
 
---
+++ js-arrays/04-exercise-removeFromArray/removeFromArray.js
@@ -0,0 +1,8 @@
+const removeFromArray = function(array, element) {
+    let index = array.indexOf(element);
+    array.splice(index, 1);
+    return array;
+};
+
+// Do not edit below this line
+module.exports = removeFromArray;
